e93eafce80e26cd35f59aaf0182da9b051034169
Ad-gangwar <128047937+Ad-gangwar@users.noreply.github.com>
2023-08-30 10:51:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,34 +1,146 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const _ = require('lodash')
+const mongoose= require("mongoose");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect('mongodb+srv://admin-aditya:password@cluster0.g1i1dhw.mongodb.net/toDoListDB', {useNewUrlParser: true});
 var item = "";
-var items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+
+// const itemShema = new mongoose.Schema({
+//     name: String,
+// });
+const itemSchema = {
+    name: String,
+};
+
+
+const itemModel= mongoose.model("Item", itemSchema);
+
+const item1 = new itemModel({
+  name: "Buy Food",
+});
+
+const item2 = new itemModel({
+  name: "Cook Food",
+});
+
+const item3 = new itemModel({
+  name: "Eat Food",
+});
+
+const defaultItems= [item1, item2, item3];
+
+const ListSchema = new mongoose.Schema({
+  name: String,
+  items: [itemSchema]
+});
+
+const listModel= mongoose.model("List", ListSchema);
+
 app.set('view engine', 'ejs');
+
 app.get("/", function (req, res) {
+  itemModel.find().then(function(foundItems){
+    if(foundItems.length===0){
+      itemModel.insertMany(defaultItems).then(function(){
+        console.log('Inserted');
+      }).catch(function(err){
+        console.log(err);
+      });
+      res.redirect();
+      
+    } else{
+    res.render("list", { listTitle: "Today", newItems: foundItems});
+    }
+  })
+  
+ //res.sendFile(__dirname + "/views/list.ejs");
+});
 
-    let day = date.getDate();
+app.get("/:customListName", function(req, res){
+  const customListName= _.capitalize(req.params.customListName);
 
-    res.render("list", { listTitle: day, newItems: items });
-    res.sendFile(__dirname + "./list.ejs");
+  const list = new listModel({
+    name: customListName,
+    items:  defaultItems,
+  }); 
+
+  listModel.findOne({name: customListName}).then(function(err, foundList){
+    if(!err){
+      if(!foundList){
+        console.log("Doesn't exist");
+        list.save();
+        res.redirect("/" + customListName);
+      }
+    } else {
+      console.log("exists!");
+      res.render("list", {listTitle: err.name, newItems: err.items});
+    }
+  });
+
 });
 
 app.post("/", function (req, res) {
-    item = req.body.newItems;
-    // console.log(item);
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect("/work");
-    } else {
-        items.push(item);
-        res.redirect("/");
+    const itemName = req.body.newItem;
+    const listName=req.body.list;
+
+    const newItem=new itemModel({
+      name: itemName,
+    });
+
+    if(listName==="Today"){
+      newItem.save();
+      res.redirect("/");
+
+    } else{
+      listModel.findOne({name: listName}).then(function(foundList){
+        foundList.items.push(newItem);
+        foundList.save();
+      });
+
+      res.redirect("/"+listName);
     }
+
+    // itemModel.insertMany(newItem);
+   
 });
+
+
+app.post("/delete", function(req, res){
+  const checkedItemId=req.body.checkbox;
+  const listName=req.body.listName;
+  console.log(listName);
+  
+  if(listName==="Today"){
+  //   itemModel.deleteOne({ _id: checkedItemId}).then(function(){
+  //     console.log("deleted"); // Success
+  //  }).catch(function(error){
+  //     console.log(error); // Failure
+  // });
+  
+  //another method
+  itemModel.findByIdAndRemove(checkedItemId).then(function(){
+    console.log("deleted");
+  }).catch(function(err){
+    console.log(err);
+  });
+
+  res.redirect("/");
+
+} else{
+  listModel.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}).then(function(){
+    console.log('deleted');
+  });
+  res.redirect("/"+listName);
+}
+
+});
+
+
 app.get("/work", function (req, res) {
     res.render("list", { listTitle: "Work List", newItems: workItems });
 });
